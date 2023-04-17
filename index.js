48cabdaa041ea3444f7f26360dd6fbf38908ba97
Brayian <bramirezag@gmail.com>
2023-04-17 14:56:12 -0500

    [feat] Add puppeter

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto("https://www.occ.com.mx/empleos/");
-
-  const jobs = await page.evaluate(function getJobInformation() {
-    const elemCardJobs = [...document.querySelectorAll('[id*="jobcard-"]')];
-    const jobs = elemCardJobs.map((cardJob) => {
-      const [
-        { href: url },
-        {
-          children: [
-            {
-              children: [
-                { innerText: fecha },
-                { innerText: title },
-                { innerText: salario },
-                { innerText: beneficios },
-                {},
-                {
-                  children: [elementEmpresCiudad],
-                },
-              ],
-            },
-          ],
-        },
-      ] = cardJob.children;
-
-      const empresa = elementEmpresCiudad?.querySelector("label")?.innerText;
-      const ciudad = elementEmpresCiudad?.querySelector("p")?.innerText;
-      return { url, fecha, title, salario, beneficios, empresa, ciudad };
-    });
-
-    return jobs;
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    arg: ["--start-maximized"],
   });
+  const page = await browser.newPage();
+  await page.goto("https://api.regulaforensics.com/?utm_source-docs");
 
-  console.log(JSON.stringify(jobs, null, 2));
+  const fileElement = await page.$(".upload-data>input[type=file]");
+  await fileElement.uploadFile("./images/dni.jpeg");
+
+  const selectorRowsInformation = "tbody > tr";
+  await page.waitForSelector(selectorRowsInformation);
+
+  const dniInformation = await page.evaluate(
+    ({ selectorRowsInformation }) => {
+      const arrayElements = [
+        ...document.querySelectorAll(selectorRowsInformation),
+      ];
+      const dniInformation = arrayElements.map((el) => {
+        const [
+          { innerText: attribute },
+          { innerText: MRZ },
+          { innerText: visualZone },
+        ] = el.children;
+        const value = visualZone != "" ? visualZone : MRZ;
+        return { attribute, value };
+      });
+      return dniInformation;
+    },
+    {
+      selectorRowsInformation,
+    }
+  );
+  console.log(JSON.stringify(dniInformation, "", 2));
 })();
